5a890a8a13f651b2dfa2599c54f987b15c35f71f
eleflea <dmzitnzc@gmail.com>
2024-06-12 08:24:25 +0800

    lambda demo

--- technology_innovation_project/technology_innovation_project_stack.py
+++ technology_innovation_project/technology_innovation_project_stack.py
@@ -1,6 +1,8 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_lambda as _lambda,
+    aws_apigateway as apigateway,
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -12,8 +14,23 @@ class TechnologyInnovationProjectStack(Stack):
 
         # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "TechnologyInnovationProjectQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Define the Lambda function resource
+        hello_world_function = _lambda.Function(
+            self,
+            "HelloWorldFunction",
+            runtime = _lambda.Runtime.PYTHON_3_11, # Choose any supported Python runtime
+            code = _lambda.Code.from_asset("technology_innovation_project/lambda"), # Points to the lambda directory
+            handler = "helloworld.lambda_handler", # Points to the 'helloworld' file in the lambda directory
+        )
+
+        # Define the API Gateway resource
+        api = apigateway.LambdaRestApi(
+            self,
+            "HelloWorldApi",
+            handler = hello_world_function,
+            proxy = False,
+        )
+        
+        # Define the '/hello' resource with a GET method
+        hello_resource = api.root.add_resource("hello")
+        hello_resource.add_method("GET")
