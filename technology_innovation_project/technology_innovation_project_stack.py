8dc830e05660f706a336e8e3f61e43eda440bfd5
eleflea <dmzitnzc@gmail.com>
2024-06-12 14:01:08 +1000

    add cloudwatch

--- technology_innovation_project/technology_innovation_project_stack.py
+++ technology_innovation_project/technology_innovation_project_stack.py
@@ -3,9 +3,14 @@ from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
     aws_apigateway as apigateway,
+    aws_cloudwatch as cw,
     # aws_sqs as sqs,
 )
+
+from aws_cdk.aws_cloudwatch import TextWidget, SingleValueWidget, GraphWidget
 from constructs import Construct
+
+import boto3
 
 class TechnologyInnovationProjectStack(Stack):
 
@@ -13,6 +18,37 @@ class TechnologyInnovationProjectStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        dashboard = cw.Dashboard(self, "Web Monitor Dashboard")
+        dashboard.add_widgets(
+            TextWidget(
+                markdown = 'Web Monitor Dashboard',
+                width = 24,
+                height = 2
+            )
+        )
+        dashboard.add_widgets(
+            SingleValueWidget(
+                metrics = [cw.Metric(
+                    metric_name = 'Test Metric',
+                    namespace = 'Monitor',
+                )],
+                width = 6,
+                height = 3,
+            )
+        )
+
+        metric_data = 6
+        metric = {
+            'MetricName': 'Test Metric',
+            'Unit': 'None',
+            'Value': metric_data
+        }
+
+        cloudwatch = boto3.client('cloudwatch', 'us-east-1')
+        cloudwatch.put_metric_data(
+            Namespace='Monitor',
+            MetricData=[metric]
+        )
 
         # Define the Lambda function resource
         hello_world_function = _lambda.Function(
