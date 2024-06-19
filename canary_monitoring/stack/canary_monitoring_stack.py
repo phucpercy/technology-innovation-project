598492723146192462cc8cf1fa243b3264343c91
eleflea <dmzitnzc@gmail.com>
2024-06-19 16:58:17 +1000

    add alarm

--- canary_monitoring/stack/canary_monitoring_stack.py
+++ canary_monitoring/stack/canary_monitoring_stack.py
@@ -1,7 +1,10 @@
 import json
 
 from aws_cdk import (
+    Stack,
+    Duration,
     aws_cloudwatch as cw,
+    aws_cloudwatch_actions as actions,
     aws_lambda as _lambda,
     aws_apigateway as apigateway,
     aws_s3 as s3, RemovalPolicy,
@@ -17,48 +20,12 @@ from aws_cdk.aws_cloudwatch import TextWidget, GraphWidget
 from constructs import Construct
 
 
-def create_metric_widgets():
-    with open('canary_monitoring/urls.json', 'r') as fr:
-        data = json.load(fr)
-    
-    widgets = []
-    for url_conf in data['urls']:
-        widgets.append(TextWidget(
-            markdown = f'## {url_conf["name"]}',
-            width = 24,
-            height = 1
-        ))
-        for metric in url_conf['metricNames']:
-            widgets.append(GraphWidget(
-                left = [cw.Metric(
-                    metric_name = metric,
-                    namespace = 'Monitor',
-                    dimensions_map= {'URL': url_conf['url']},
-                )],
-                width = 6,
-                height = 4,
-            ))
-    return widgets
-
-
 class CanaryMonitoringStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
-
-        # Custom CloudWatch dashboard
-        dashboard = cw.Dashboard(self, "Web Monitor Dashboard")
-        dashboard.add_widgets(
-            TextWidget(
-                markdown = '# Web Monitor Dashboard',
-                width = 24,
-                height = 2
-            )
-        )
-        widgets = create_metric_widgets()
-        dashboard.add_widgets(*widgets)
 
         # Define the Lambda function resource
         monitoring_function = _lambda.Function(
@@ -97,7 +64,7 @@ class CanaryMonitoringStack(Stack):
         monitoring_scheduled_rule = events.Rule(
             self,
             f'EventBridgeRule',
-            schedule=events.Schedule.rate(Duration.minutes(30)),
+            schedule=events.Schedule.rate(Duration.minutes(1)),
             targets=[events_targets.LambdaFunction(handler=monitoring_function,)],
             rule_name=f'MonitoringSchedule',
         )
@@ -160,3 +127,50 @@ class CanaryMonitoringStack(Stack):
             description='The SNS notification-topic ARN for test'
         )
 
+        # Custom CloudWatch dashboard
+        dashboard = cw.Dashboard(self, "Web Monitor Dashboard")
+        dashboard.add_widgets(
+            TextWidget(
+                markdown = '# Web Monitor Dashboard',
+                width = 24,
+                height = 2
+            )
+        )
+        widgets = self.create_metric_widgets(sns_alarm_topic)
+        dashboard.add_widgets(*widgets)
+
+    def create_metric_widgets(self, topic: sns.Topic):
+        with open('canary_monitoring/urls.json', 'r') as fr:
+            data = json.load(fr)
+        
+        widgets = []
+        for url_conf in data['urls']:
+            widgets.append(TextWidget(
+                markdown = f'## {url_conf["name"]}',
+                width = 24,
+                height = 1
+            ))
+            for metric_name in url_conf['metricNames']:
+                metric = cw.Metric(
+                    metric_name = metric_name,
+                    namespace = 'Monitor',
+                    dimensions_map= {'URL': url_conf['url']},
+                    period=Duration.minutes(1),
+                )
+                if metric_name == 'Page Time':
+                    alarm: cw.Alarm = metric.create_alarm(
+                        self,
+                        f'Alarm-{metric_name}-{url_conf["url"]}',
+                        threshold=0.5,
+                        evaluation_periods=2,
+                        datapoints_to_alarm=1,
+                        comparison_operator=cw.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD
+                    )
+                    alarm.add_alarm_action(actions.SnsAction(topic))
+                widgets.append(GraphWidget(
+                    left = [metric],
+                    width = 6,
+                    height = 4,
+                ))
+
+        return widgets
